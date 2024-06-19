49718f9df47b30a0fc7ad0c88ff09f753ce74e8b
Juliette Cordor <professional@maybejules.com>
2024-06-19 16:58:00 +1000

    added map benchmarks

--- benches/env.rs
+++ benches/env.rs
@@ -1,8 +1,10 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use std::collections::{BTreeMap, HashMap};
+
+use criterion::{black_box, criterion_group, criterion_main, BatchSize, Criterion};
 
 use safenv as env;
 
-fn criterion_benchmark(c: &mut Criterion) {
+fn benchmark_env(c: &mut Criterion) {
     c.bench_function("inherit env", |b| b.iter(|| unsafe { env::inherit() }));
 
     c.bench_function("inherit env black_box", |b| {
@@ -10,5 +12,52 @@ fn criterion_benchmark(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, criterion_benchmark);
+fn benchmark_maps(c: &mut Criterion) {
+    fn default_hashmap() -> HashMap<String, String> {
+        let mut map = HashMap::new();
+        map.insert(black_box("FOO".to_string()), black_box("BAR".to_string()));
+        map
+    }
+
+    fn default_btreemap() -> BTreeMap<String, String> {
+        let mut map = BTreeMap::new();
+        map.insert(black_box("FOO".to_string()), black_box("BAR".to_string()));
+        map
+    }
+
+    c.bench_function("default hashmap", |b| b.iter(default_hashmap));
+    c.bench_function("default btreemap", |b| b.iter(default_btreemap));
+
+    c.bench_function("insert hashmap", |b| {
+        b.iter_batched(
+            default_hashmap,
+            |mut map| map.insert(black_box("BAR".to_string()), black_box("FOO".to_string())),
+            BatchSize::SmallInput,
+        );
+    });
+    c.bench_function("insert btreemap", |b| {
+        b.iter_batched(
+            default_btreemap,
+            |mut map| map.insert(black_box("BAR".to_string()), black_box("FOO".to_string())),
+            BatchSize::SmallInput,
+        );
+    });
+
+    c.bench_function("remove hashmap", |b| {
+        b.iter_batched(
+            default_hashmap,
+            |mut map| map.remove(black_box("FOO")),
+            BatchSize::SmallInput,
+        );
+    });
+    c.bench_function("remove btreemap", |b| {
+        b.iter_batched(
+            default_btreemap,
+            |mut map| map.remove(black_box("FOO")),
+            BatchSize::SmallInput,
+        );
+    });
+}
+
+criterion_group!(benches, benchmark_env, benchmark_maps);
 criterion_main!(benches);
